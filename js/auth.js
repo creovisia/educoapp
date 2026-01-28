b0d3f09f2860d896f3487e41cc3ce8e3db5ac7e6
creovisia <mukilkarkimail@gmail.com>
2026-01-28 21:28:25 +0530

    update

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDcXgGY5zwFCuDByvpgt6GzF5B2M9uZdFc",
-  authDomain: "educoapp.firebaseapp.com",
-  databaseURL: "https://educoapp-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "educoapp",
-  storageBucket: "educoapp.firebasestorage.app",
-  messagingSenderId: "727036537455",
-  appId: "1:727036537455:web:5bf727d17ed8f5bf153b9a",
-  measurementId: "G-2KYGR2XWP6"
-};
+
 
 
 import { auth } from "../firebase.js";
--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,19 @@
-<script type="module">
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyDcXgGY5zwFCuDByvpgt6GzF5B2M9uZdFc",
+  authDomain: "educoapp.firebaseapp.com",
+  databaseURL: "https://educoapp-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "educoapp",
+  storageBucket: "educoapp.firebasestorage.app",
+  messagingSenderId: "727036537455",
+  appId: "1:727036537455:web:5bf727d17ed8f5bf153b9a",
+  measurementId: "G-2KYGR2XWP6"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-</script>
