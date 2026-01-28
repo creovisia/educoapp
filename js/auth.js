489465cc350a96a5266e75b5f5a6cdbc6ddc2891
creovisia <mukilkarkimail@gmail.com>
2026-01-28 21:18:40 +0530

    update

--- js/auth.js
+++ js/auth.js
@@ -9,3 +9,24 @@ const firebaseConfig = {
   appId: "1:727036537455:web:5bf727d17ed8f5bf153b9a",
   measurementId: "G-2KYGR2XWP6"
 };
+
+
+import { auth } from "../firebase.js";
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = function () {
+  signInWithEmailAndPassword(
+    auth,
+    email.value,
+    password.value
+  ).then(() => {
+    window.location.href = "home.html";
+  }).catch(err => alert(err.message));
+};
+
+onAuthStateChanged(auth, user => {
+  if (user && location.pathname.includes("index")) {
+    window.location.href = "home.html";
+  }
+});
