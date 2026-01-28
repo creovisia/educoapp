648fd6b5ad0e0ef58ffe5eb1339a2688030a6c4b
creovisia <mukilkarkimail@gmail.com>
2026-01-28 07:08:45 +0530

    update

--- js/auth.js
+++ js/auth.js
@@ -1,22 +1,11 @@
-import { auth } from "./firebase.js";
-import {
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-window.login = () => {
-  signInWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => location.href = "home.html")
-    .catch(e => alert(e.message));
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDcXgGY5zwFCuDByvpgt6GzF5B2M9uZdFc",
+  authDomain: "educoapp.firebaseapp.com",
+  databaseURL: "https://educoapp-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "educoapp",
+  storageBucket: "educoapp.firebasestorage.app",
+  messagingSenderId: "727036537455",
+  appId: "1:727036537455:web:5bf727d17ed8f5bf153b9a",
+  measurementId: "G-2KYGR2XWP6"
 };
-
-window.logout = () => {
-  signOut(auth).then(() => location.href = "index.html");
-};
-
-export function protect() {
-  onAuthStateChanged(auth, user => {
-    if (!user) location.href = "index.html";
-  });
-}
